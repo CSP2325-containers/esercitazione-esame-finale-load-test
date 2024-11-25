1af0fc3060fabc82857a228b4ed90911b928c123
Gabriele Cavigiolo <gabri.cavi@gmail.com>
2024-11-25 15:45:03 +0100

    Locust commit

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,32 +1,24 @@
+import os
 from locust import HttpUser, TaskSet, task, between
 from bs4 import BeautifulSoup
 
 
 class CCLUser(HttpUser):
     wait_time = between(1, 5)  # Simula un tempo di attesa tra 1 e 5 secondi
-    host = "https://backoffice.cassacommercioliguria.it"
+    host = os.getenv("LOCUST_HOST", "http://localhost")
     
     @task
-    def view_application_crud(self):
-        self.client.get("/?crudAction=index&crudControllerFqcn=App%5CController%5CAdmin%5CApplicationCrudController")
+    def view_home(self):
+        self.client.get("/")
     
     @task
-    def view_application_group_420(self):
-        self.client.get("/?crudAction=edit&crudControllerFqcn=App%5CController%5CAdmin%5CApplicationGroupCrudController&entityId=420")
+    def view_health_db(self):
+        self.client.get("/health-db")
     
     @task
-    def view_application_detail(self):
-        self.client.get("/?crudAction=detail&crudControllerFqcn=App%5CController%5CAdmin%5CApplicationCrudController&entityId=1206&page=1&sort%5Bid%5D=DESC")
+    def view_user_list(self):
+        self.client.get("/users")
         
     @task
-    def view_application_detail_2(self):
-        self.client.get("/?crudAction=detail&crudControllerFqcn=App%5CController%5CAdmin%5CApplicationCrudController&entityId=1202&page=1&sort%5Bid%5D=DESC")    
-    
-    @task
-    def view_application_group(self):
-        self.client.get("/?crudAction=index&crudControllerFqcn=App%5CController%5CAdmin%5CApplicationGroupCrudController") 
-    
-    @task(3)
-    def eb_health_check(self):
-        self.client.get("/elbHealthCheck")
-        
+    def read_first_user(self):
+        self.client.get("/users/1")     
